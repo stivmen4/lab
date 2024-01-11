5c9cd826a2c741116f74a6906a38b89cd5bf34a4
stivmen4 <108321695+stivmen4@users.noreply.github.com>
2024-01-11 15:24:27 +0200

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,37 @@
+
+# 1. Функція перевірки на парність/непарність числа
+def check_number(num):
+    if isinstance(num, (int, float)):
+        return "Парне" if num % 2 == 0 else "Непарне"
+    else:
+        return ""
+
+# 2. Функція знаходження суми перших п'яти простих чисел
+def sum_of_primes():
+    primes = []
+    num = 2
+    while len(primes) < 5:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            primes.append(num)
+        num += 1
+    return sum(primes)
+
+# 3. Функція для обчислення суми ряду
+def sum_of_series(n):
+    result = 0
+    for i in range(1, n + 1):
+        number = int('1' * i)
+        result += number
+    return result
+
+# Приклади викликів функцій:
+print(check_number(10))  # Виведе "Парне"
+print(check_number(7))   # Виведе "Непарне"
+print(check_number("abc"))  # Виведе ""
+
+print(sum_of_primes())   # Знайде суму перших п'яти простих чисел
+
+print(sum_of_series(5))  # Виведе 12345 
